1f4fac85a2124916722c04e43dfd656574da1a02
Vencislav <vencislav67@hotmail.com>
2022-05-21 12:36:11 +0300

    The problem Was solved 100/100. Should Train more with these kind of matchmaking problems!

--- 08_BalancedParentheses/Program.cs
+++ 08_BalancedParentheses/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _08_BalancedParentheses
 {
@@ -6,7 +9,71 @@ namespace _08_BalancedParentheses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*string input = Console.ReadLine();
+            Stack<char> parenthesStack = new Stack<char>();
+            foreach (var symbol in input)
+            {
+                if (parenthesStack.Any())
+                {
+                    char check = parenthesStack.Peek();
+                    if (check == '{' && symbol == '}')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                    else if (check == '[' && symbol == ']')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                    else if (check == '(' && symbol == ')')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                }
+                parenthesStack.Push(symbol);
+            }
+            Console.WriteLine(!parenthesStack.Any() ? "YES" : "NO");*/
+
+            String parentheses = Console.ReadLine();
+            Stack<char> sPare = new Stack<char>();
+
+            for(int i= 0; i < parentheses.Length; i++)
+            {
+                char c = parentheses[i];
+
+                if (sPare.Count != 0)
+                {
+                    
+                    if (c == ')' && sPare.Peek() == '(')
+                    {
+                        sPare.Pop();
+                        continue;
+                    }
+                    else if (c == ']' && sPare.Peek() == '[')
+                    {
+                        sPare.Pop();
+                        continue;
+                    }
+                    else if (c == '}' && sPare.Peek() == '{')
+                    {
+                        sPare.Pop();
+                        continue;
+                    }
+                }
+                sPare.Push(c);
+                
+            }
+
+            if(sPare.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
