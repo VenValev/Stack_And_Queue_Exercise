9065d7cdd40f4d76a4a1e454596af191811e2d71
Vencislav <vencislav67@hotmail.com>
2022-05-21 13:26:15 +0300

    Started the text manipulation.

--- 09_SimpleTextEditor/Program.cs
+++ 09_SimpleTextEditor/Program.cs
@@ -6,7 +6,11 @@ namespace _09_SimpleTextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+
+            }
         }
     }
 }
