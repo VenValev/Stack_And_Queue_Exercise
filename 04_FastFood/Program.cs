29b8965721c5e53ee70733d400b9813e0a02befd
Vencislav <vencislav67@hotmail.com>
2022-05-22 19:44:37 +0300

    Added solution to propblem N4

---
+++ 04_FastFood/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+
+            List<int> orders = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            Queue<int> foodOrders = new Queue<int>(orders);
+            int fo = foodOrders.Count;
+            Console.WriteLine(foodOrders.Max());
+
+            for (int i = 0; i < fo; i++)
+            {
+                if (food >= foodOrders.Peek())
+                {
+                    food -= foodOrders.Peek();
+                    foodOrders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (foodOrders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", foodOrders)}");
+
+            }
+
+
+        }
+    }
+}
