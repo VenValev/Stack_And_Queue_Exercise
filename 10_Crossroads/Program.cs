191b2e6b0b567c9e7076677397fcb1b569f4c860
Vencislav <vencislav67@hotmail.com>
2022-05-22 13:12:09 +0300

    Made Changes to the cmnd and the ending of the program.

--- 10_Crossroads/Program.cs
+++ 10_Crossroads/Program.cs
@@ -15,9 +15,11 @@ namespace _10_Crossroads
             //Queue<char> lastCar = new Queue<char>();
             //Stack<char> s = new Stack<char>();
 
-            while((cmnd = Console.ReadLine()) != "END")
+            while(true)
             {
-                if(cmnd == "green")
+                cmnd = Console.ReadLine();
+
+                if (cmnd == "green")
                 {
                     for(int i = 0; i < greenLightTime; i++)
                     {
@@ -33,15 +35,20 @@ namespace _10_Crossroads
                         {
                             Console.WriteLine("A crash happened!");
                             Console.WriteLine($"{lastCar} was hit at{lastCar[freeWindow]}");
-
+                            break;
                         }
                     }
+                }
+                else if( cmnd == "END")
+                {
+                    Console.WriteLine("");
                 }
                 else
                 {
                     cars.Enqueue(cmnd);
                 }
             }
+
         }
 
          static void FillingTheLastCarQueue(Queue<char> lastCar, char[] c)
