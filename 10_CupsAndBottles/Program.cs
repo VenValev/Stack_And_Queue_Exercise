9bd01fc1d4367c05811d12b83fc4475aa279dce7
Vencislav <vencislav67@hotmail.com>
2022-05-22 18:21:26 +0300

    Started the alghoritm for filling the cups

--- 10_CupsAndBottles/Program.cs
+++ 10_CupsAndBottles/Program.cs
@@ -9,12 +9,39 @@ namespace _10_CupsAndBottles
     {
         static void Main(string[] args)
         {
-            Queue<int> numberOfCups = new Queue<int>(Console.ReadLine()
+            Queue<int> capacityOfCups = new Queue<int>(Console.ReadLine()
                 .Split(' ')
                 .Select(int.Parse));
-            Stack<int> numberOfBottles = new Stack<int>(Console.ReadLine()
+            Stack<int> capacityOfBottles = new Stack<int>(Console.ReadLine()
                 .Split(' ')
                 .Select(int.Parse));
+            int wastedWater = 0;
+
+            while(true)
+            {
+                int currentCup = capacityOfCups.Peek();
+                int currentBottle = capacityOfBottles.Peek();
+
+                if (capacityOfCups.Count == 0 && capacityOfBottles.Count > 0)
+                {
+
+                }
+                else if (capacityOfBottles.Count == 0 && capacityOfCups.Count > 0)
+                {
+
+                }
+                else
+                {
+                    while(currentCup > 0)
+                    {
+                        currentCup -= capacityOfBottles.Pop();
+                        if(currentCup < 0)
+                        {
+                            wastedWater += Math.Abs(currentCup);
+                        }
+                    }
+                }
+            }
         }
     }
 }
