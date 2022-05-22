0eb089e863214fc578671c0e2f92fdb304b7a0cf
Vencislav <vencislav67@hotmail.com>
2022-05-22 18:00:04 +0300

    Started A New project n12

---
+++ 10_CupsAndBottles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10_CupsAndBottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
