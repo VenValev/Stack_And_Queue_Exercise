1ea84f610795e85ac1787d397e214226fb54fc7f
Vencislav <vencislav67@hotmail.com>
2022-05-22 16:09:02 +0300

    Started a new Project n11. Started initializing the variables. GL.

---
+++ 11_KeyRevolver/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _11_KeyRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int priceOfBullet = int.Parse(Console.ReadLine());
+            int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
