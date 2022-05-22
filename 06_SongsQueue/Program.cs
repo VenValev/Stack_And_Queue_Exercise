ff9a9f25b229b1ba430b602fa9570934fc4fa16a
Vencislav <vencislav67@hotmail.com>
2022-05-22 19:47:42 +0300

    Added solution to problem N6 (this time for real! the last one was solution N5)

---
+++ 06_SongsQueue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> playlist = new Queue<string>(Console.ReadLine()
+                .Split(", "));
+
+            while (playlist.Count > 0)
+            {
+                string[] cmnd = Console.ReadLine().Split(' ', 2);
+
+                if (cmnd[0] == "Play")
+                {
+                    playlist.Dequeue();
+                }
+                else if (cmnd[0] == "Add")
+                {
+
+                    if (playlist.Contains(cmnd[1]))
+                    {
+                        Console.WriteLine($"{cmnd[1]} is already contained!");
+                    }
+                    else
+                    {
+                        playlist.Enqueue(cmnd[1]);
+
+                    }
+                }
+                else if (cmnd[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", playlist));
+                }
+
+                if (playlist.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+            }
+        }
+    }
+}
